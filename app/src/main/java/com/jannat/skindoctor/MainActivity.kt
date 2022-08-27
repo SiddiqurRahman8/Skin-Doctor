a188dc06d05d4905fbe8ed8219b58d25fd3e928f
Md Siddiqur Rahman <mdsiddiqur805@gmail.com>
2022-08-27 18:26:18 +0600

    Skin Doctor

---
+++ app/src/main/java/com/jannat/skindoctor/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.jannat.skindoctor
+//supportActionBar?.hide()
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mPhotoImageView: ImageView
+    private lateinit var mCameraButton: Button
+    private lateinit var mGalleryButton: Button
+    private lateinit var mDetectButton: Button
+    private lateinit var Result: TextView
+    private lateinit var percent: TextView
+    private lateinit var mClassifier: Classifier
+    private lateinit var mBitmap: Bitmap
+
+    private val mCameraRequestCode = 0
+    private val mGalleryRequestCode = 2
+
+    private val mInputSize = 224
+    private val mModelPath = "model.tflite"
+    private val mLabelPath = "labels.txt"
+    private val mSamplePath = "skin_icon.jpg"
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        setContentView(R.layout.activity_main)
+        mPhotoImageView=findViewById(R.id.mPhotoImageView)
+        mCameraButton=findViewById(R.id.mCameraButton)
+        mGalleryButton=findViewById(R.id.mGalleryButton)
+        mDetectButton=findViewById(R.id.mDetectButton)
+
+        Result=findViewById(R.id.ResultTV)
+
+        percent=findViewById(R.id.percentTV)
+        mClassifier = Classifier(assets, mModelPath, mLabelPath, mInputSize)
+
+        resources.assets.open(mSamplePath).use {
+            mBitmap = BitmapFactory.decodeStream(it)
+            mBitmap = Bitmap.createScaledBitmap(mBitmap, mInputSize, mInputSize, true)
+            mPhotoImageView.setImageBitmap(mBitmap)
+        }
+
+        mCameraButton.setOnClickListener {
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(callCameraIntent, mCameraRequestCode)
+        }
+
+        mGalleryButton.setOnClickListener {
+            val callGalleryIntent = Intent(Intent.ACTION_PICK)
+            callGalleryIntent.type = "image/*"
+            startActivityForResult(callGalleryIntent, mGalleryRequestCode)
+        }
+        mDetectButton.setOnClickListener {
+            val results = mClassifier.recognizeImage(mBitmap).firstOrNull()
+            Result.text= "Class Name : "+results?.title+"\n Accuracy : "+results?.confidence
+            percent.setVisibility(View.VISIBLE)
+
+
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == mCameraRequestCode){
+
+            if(resultCode == Activity.RESULT_OK && data != null) {
+                mBitmap = data.extras!!.get("data") as Bitmap
+                mBitmap = scaleImage(mBitmap)
+                val toast = Toast.makeText(this, ("Image crop to: w= ${mBitmap.width} h= ${mBitmap.height}"), Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.BOTTOM, 0, 20)
+                toast.show()
+                mPhotoImageView.setImageBitmap(mBitmap)
+                Result.text= "Your photo image set now."
+                percent.setVisibility(View.INVISIBLE)
+
+            } else {
+                Toast.makeText(this, "Camera cancel..", Toast.LENGTH_LONG).show()
+            }
+        } else if(requestCode == mGalleryRequestCode) {
+            if (data != null) {
+                val uri = data.data
+
+                try {
+                    mBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                println("Success!!!")
+                mBitmap = scaleImage(mBitmap)
+                mPhotoImageView.setImageBitmap(mBitmap)
+
+            }
+        } else {
+            Toast.makeText(this, "Unrecognized request code", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+
+    fun scaleImage(bitmap: Bitmap?): Bitmap {
+        val orignalWidth = bitmap!!.width
+        val originalHeight = bitmap.height
+        val scaleWidth = mInputSize.toFloat() / orignalWidth
+        val scaleHeight = mInputSize.toFloat() / originalHeight
+        val matrix = Matrix()
+        matrix.postScale(scaleWidth, scaleHeight)
+        return Bitmap.createBitmap(bitmap, 0, 0, orignalWidth, originalHeight, matrix, true)
+
+    }
+
+}
---
+++ app/src/main/java/com/jannat/skindoctor/SplashActivity.kt
@@ -0,0 +1,41 @@
+package com.jannat.skindoctor
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        val motionLayout = findViewById<MotionLayout>(R.id.motionLayout)
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+        })
+
+    }
+
+}
+
